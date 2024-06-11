4c5df53c55bf005373a6f9de2a10f024791f72c5
bosMC <bostanAF@gmail.com>
2024-06-11 21:44:24 +0700

    change start files to my_version

---
+++ additional_function_for_app.py
@@ -0,0 +1,67 @@
+import requests
+
+import os
+import json
+from datetime import datetime
+
+
+def check_correctly_date(user_date_string: str) -> bool:
+    try:
+        datetime.strptime(user_date_string, '%d.%m.%Y')
+        return True
+    except ValueError:
+        return False
+
+
+def save_data_about_currency(user_data: datetime) -> bool:
+    """Документ строка"""
+    # вставить в нужные места дату которые ввел пользователь.(что бы это работало)
+    url = f'https://www.cbr-xml-daily.ru/archive/{user_data.strftime("%Y/%m/%d")}/daily_json.js'
+
+    response = requests.get(url)
+    status = response.status_code
+
+    if status != 200:
+        return False
+    else:
+        file_name = user_data.strftime(f'%d_%m_%Y')
+        with open(f"currency/{file_name}.json", 'w', encoding='utf-8') as file:
+            json.dump(response.json(), file, indent=4, ensure_ascii=False)
+    return True
+
+
+# 1) пользователь может ввести не правильную дату
+# 1.1) несуществующий фактически в календаре день
+# 1.2) некорректный вид день.месяц.год -> месяц.день.год
+# 2) пользователь может постоянно спрашивать одну и ту же дату
+# - запросы которые мы прошли успешно ()
+# - запросы, которые возвращают код 4.. - их было неплохо где-то сохранять
+
+
+# 0) где и как хранить успешные и неуспешные запросы?
+
+# 14.05.2024 -> 14_05_2024.json
+# 1) принять от пользователя строку и перевести её в более удобный для себя формат
+# 2) заглянуть в папку
+
+# files = os.listdir('currency')
+# print(files)
+
+# json_object = {
+#     '14_05_2024': True,
+#     '15_05_2024': True,
+#     '17_05_2024': True,
+# }
+#
+# # text = 'h\\w'             # набор символов
+# # print((len(text)))
+# # print(text)
+#
+#
+# def func(a, b, c, d):
+#     return 'hello'
+#
+#
+# lst = [1, 2, 3, 4]
+# a = lst[-1]
+# print(a)
--- main.c
+++
@@ -1,6 +0,0 @@
-hello world
-
-git add .
-git commit -m 'комментарий'
-git push		- отправка изменений на удаленный
-git pull		- стянуть изменения из удвленного
