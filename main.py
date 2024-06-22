0aec9f61b664ba02c12b65a8dedbef5c202acdbd
bosMC <bostanAF@gmail.com>
2024-06-22 22:51:07 +0700

    добавлено чтение файла

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 
 from additional_function_for_app import check_correctly_date, get_answer_about_next_response
-
+from additional_function_for_app import get_right_path
 
 user_date_string = input('Введи дату: ')
 
@@ -13,16 +13,15 @@ if check_correctly_date(user_date_string):
     key: str = user_date.strftime('%d_%m_%Y')
 
     result = get_answer_about_next_response(user_date, key)
+    if result == 'Данные найдены!':
+        with open(f'{get_right_path()}{key}.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            print(data)
 
     print(result)
 
 else:
     print('Ты ввел что-то не похожее на дату')
-
-
-
-
-
 
 
 
