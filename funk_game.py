88f6e2057311fd2874ffc649e6a0cc76fbb472b3
STAIN <denisbebih@gmail.com>
2024-06-22 18:51:23 +0300

    решена проблема

--- funk_game.py
+++ funk_game.py
@@ -40,5 +40,50 @@ def save_data_about_currency(user_input: str) -> bool:
 
 
 
+def get_right_path(folder_name: str = 'currency') -> str:
+    """Функция призвана сформировать абсолютный путь до папки,
+        которая указана в качестве аргумента в зависимости от ОС, на которой выполняется запуск"""
+    path_to_current_file = os.path.abspath(__file__)
+    seperator = '/' if os.name == 'posix' else '\\'
+    path_list = path_to_current_file.split(seperator)
+
+    path_list[-1] = folder_name
+
+    return seperator.join(path_list) + seperator
+
+def get_answer_about_next_response(user_date: datetime, key: str) -> str:
+    info_response: dict = get_info_about_response(key)
+    completed_response: dict = info_response['all_data']
+
+    if info_response['flag'] == 'Требуется выполнить запрос!':
+        flag_response: bool = save_data_about_currency(user_date)
+        completed_response[key] = flag_response
+
+        save_all_responses(completed_response)
+
+    return info_response['flag']
+
+def get_info_about_response(data_key: str) -> dict:
+    with open(f'{get_right_path()}completed_response.json', 'r', encoding='utf-8') as file:
+        completed_response: dict = json.load(file)
+
+    result_dict: dict = {
+        'flag': '',
+        'all_data': completed_response,
+    }
+
+    if data_key in completed_response:
+        flag = completed_response[data_key]
+        if flag:
+            result_dict['flag'] = 'Данные найдены!'
+        else:
+            result_dict['flag'] = 'По дате на бирже нету данных!'
+    else:
+        result_dict['flag'] = 'Требуется выполнить запрос!'
+
+    return result_dict
 
 
+def save_all_responses(all_data: dict) -> None:
+    with open(f'{get_right_path()}completed_response.json', 'w', encoding='utf-8') as file:
+        json.dump(all_data, file, indent=4, ensure_ascii=False)
--- main.py
+++ main.py
@@ -11,16 +11,7 @@
 # b = a['Valute']['AUD']['Name']
 #
 # print(b)
-
-
-
-
-
-
-
-
-
-
+import json
 # 1. мы получали ответ в формате json
 # 2. проверяли корректность полученных данных
 # 3. либо вывели сообщение об ошибке
@@ -51,31 +42,30 @@
 
 
 
-
-
-
-import json
 from datetime import datetime
 
-from funk_game import save_data_about_currency, check_currrency_data, getrightpath
+from funk_game import check_currrency_data, get_answer_about_next_response,get_right_path
 
 
 user_date_string = input('Введи дату: ')
-if check_currrency_data(user_date_string):
-    user_date_datetime = datetime.strptime(user_date_string, '%d.%m.%Y')
-    with open(f'{getrightpath()}completed_response.json', 'r', encoding='utf-8') as file:
-        completed_response: dict = json.load(file)
-        key = user_date_datetime.strftime('%d%m_%Y')
-        if key in completed_response:
-            print('ничего никуда не отправляется!')
-        else:
-            response_result = save_data_about_currency(user_date_datetime)
-            completed_response[key] = response_result
 
-    with open(f'{getrightpath()}completed_response.json', 'w', encoding='utf-8') as file:
-        json.dump(completed_response, file, indent=4, ensure_ascii=False)
+if check_currrency_data(user_date_string):
+    user_date: datetime = datetime.strptime(user_date_string, '%d.%m.%Y')
+    key: str = user_date.strftime('%d_%m_%Y')
+
+    result = get_answer_about_next_response(user_date, key)
+    if result == 'Данные найдены!':
+        with open(f"{get_right_path()}{key}.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+            print(data)
+
+    print(result)
+
 else:
     print('Ты ввел что-то не похожее на дату')
+
+
+
 
 
 
